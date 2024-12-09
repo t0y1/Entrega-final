738fbe56837ae7489847deabccd20b7513496a43
Lautaro Urso <lautarourso@gmail.com>
2024-12-09 15:48:18 -0300

    BASTA

--- index.js
+++ index.js
@@ -17,5 +17,5 @@ app.use("/auth", AuthRouter);
 app.use("/pedidos", PedidosRouter);
 
 app.listen(process.env.PORT || 9000, () =>
-    console.log(`Server is running on port ${process.env.PORT || 9000}`)
+    console.log(`Server is running on port ${process.env.PORT || 9000}` + ", Ya puedes empezar")
 );
